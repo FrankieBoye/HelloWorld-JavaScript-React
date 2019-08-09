726a803ed43c33eacdd76850b8892ca97ef6e90a
FrankieBoye <frankie4@btinternet.com>
2019-08-09 22:37:09 +0100

    removed instructions from page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         <h1>Hello World!</h1>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
